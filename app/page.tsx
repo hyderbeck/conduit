ff44f9bd068c7afc6a242209da8b3a5d8a7027b9
Hyder Beck <hyderbeck9@gmail.com>
2024-01-23 16:17:34 +0300

    feat: draft homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,26 @@ export default function Page() {
   return (
     <main>
       <h2>Conduit</h2>
+      <p>A place to share your knowledge.</p>
+      <section>
+        <ul>
+          <li>
+            <h3>Feed</h3>
+          </li>
+        </ul>
+        <article>article</article>
+      </section>
+      <nav aria-label="pagination">
+        <ul>
+          <li>1</li>
+        </ul>
+      </nav>
+      <section>
+        <h3>Popular Tags</h3>
+        <ul>
+          <li>tag</li>
+        </ul>
+      </section>
     </main>
   );
 }
