f7f2eff13324a5a981a7c84e746de29595287f65
Hyder Beck <hyderbeck9@gmail.com>
2024-01-23 16:28:20 +0300

    feat(header): draft nav

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,36 @@
+import Link from "next/link";
+
 export default function Header() {
+  const user = "username";
+  const userMenu = true;
+
   return (
     <header>
       <h1>Conduit</h1>
+      {user ? (
+        <nav>
+          <ul>
+            <li>
+              <Link href="/new">New Article</Link>
+            </li>
+            <li>
+              <button>{user}</button>
+              {userMenu && (
+                <ul>
+                  <li>
+                    <Link href={`/${user}`}>Profile</Link>
+                  </li>
+                  <li>
+                    <button>Log Out</button>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <button>Log In</button>
+      )}
     </header>
   );
 }
