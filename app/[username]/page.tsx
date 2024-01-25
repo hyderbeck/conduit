4107e4f035d58a10759ab4e5bd5c0e413ed94492
Hyder Beck <hyderbeck9@gmail.com>
2024-01-26 00:11:26 +0300

    feat(profile): get data

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,10 +1,30 @@
 import Feed from "@/components/feed";
+import { createClient } from "@/supabase";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
 
-export default function Page() {
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const profile = (
+    await createClient(cookies())
+      .schema("conduit")
+      .from("profiles")
+      .select()
+      .eq("username", params.username)
+      .single()
+  ).data;
+
+  if (!profile) {
+    notFound();
+  }
+
   return (
     <main>
-      <h2>username</h2>
-      <p>bio</p>
+      <h2>{profile.username}</h2>
+      {profile.bio && <p>{profile.bio}</p>}
       <button>Follow</button>
       <ul>
         <li>
