63bab5119cb22fcb030d4eea72b22046547a697b
Hyder Beck <hyderbeck9@gmail.com>
2024-01-25 23:44:15 +0300

    feat(feed): get articles

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,17 +1,62 @@
-export default function Feed() {
+import { createClient } from "@/supabase";
+import { cookies } from "next/headers";
+import { Tables } from "@/types";
+import Link from "next/link";
+
+function Article({
+  article,
+  author,
+}: {
+  article: Tables<"articles">;
+  author: string;
+}) {
+  return (
+    <article>
+      <Link href={`/${author}`}>
+        <address>{author}</address>
+      </Link>
+      <time>{article.created_at}</time>
+      <button>Like</button>
+      <h4>{article.title}</h4>
+      <p>{article.body}</p>
+      <p>
+        <Link href={`/${author}/${article.slug}`}>Read more...</Link>
+      </p>
+      {article.tag_list && (
+        <ul>
+          {article.tag_list?.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      )}
+    </article>
+  );
+}
+
+export default async function Feed() {
+  const supabase = createClient(cookies());
+  const articles = (await supabase.schema("conduit").from("articles").select())
+    .data;
+
   return (
     <section>
-      <article>
-        <address>author</address>
-        <time>date</time>
-        <button>Like</button>
-        <h4>title</h4>
-        <p>body</p>
-        <p>Read more...</p>
-        <ul>
-          <li>tag</li>
-        </ul>
-      </article>
+      {articles?.length &&
+        articles?.map(async (article) => (
+          <Article
+            key={article.id}
+            article={article}
+            author={
+              (
+                await supabase
+                  .schema("conduit")
+                  .from("profiles")
+                  .select()
+                  .eq("user_id", article.author_id)
+                  .single()
+              ).data!.username
+            }
+          />
+        ))}
       <nav aria-label="pagination">
         <ul>
           <li>1</li>
