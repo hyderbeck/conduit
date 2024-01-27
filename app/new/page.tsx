38ac5b37898f20e108fc37f0abef976a8b1b0263
Hyder Beck <hyderbeck9@gmail.com>
2024-01-27 06:32:18 +0300

    feat: allow to create new articles

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,12 +1,46 @@
-export default function Page() {
+import { createClient } from "@/supabase";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Article from "../[username]/[slug]/article";
+import { updateArticle } from "@/app/actions";
+
+export default async function Page() {
+  const supabase = createClient(cookies());
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect(`/?login=true`);
+  }
+
+  const username = (
+    await supabase
+      .schema("conduit")
+      .from("profiles")
+      .select()
+      .eq("user_id", user.id)
+      .single()
+  ).data!.username;
+
   return (
-    <main>
-      <h2>title</h2>
-      <address>author</address>
-      <p>body</p>
-      <ul>
-        <li>tag</li>
-      </ul>
-    </main>
+    <Article
+      article={{
+        author_id: user.id,
+        body: "",
+        created_at: "",
+        slug: "",
+        title: "",
+        tag_list: [],
+        description: "",
+        id: 0,
+        updated_at: "",
+      }}
+      username={username}
+      userId={user.id}
+      updateArticle={updateArticle}
+      isNew
+    />
   );
 }
