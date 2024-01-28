20adcb8898a3a029e241469318d493f0fb49341a
Hyder Beck <hyderbeck9@gmail.com>
2024-01-28 14:40:08 +0300

    feat: add global styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { roboto } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Conduit",
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${roboto.className} bg-stone-50 text-stone-950 flex flex-col min-h-screen`}
+      >
         <Header />
         {children}
         <Footer />
