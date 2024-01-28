c194f1c637c3f7a6dc518caddeda7b40bca1fb89
Hyder Beck <hyderbeck9@gmail.com>
2024-01-29 01:10:37 +0300

    feat: update footer info & styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>Conduit</p>
+    <footer className="bg-stone-100 mt-auto">
+      <div className="px-6 py-4 flex gap-x-4 items-center max-w-screen-lg mx-auto">
+        <p className="text-emerald-300 text-lg font-black">conduit</p>
+        <p className="text-stone-400 text-sm">
+          Code &amp; design licensed under MIT.
+        </p>
+      </div>
     </footer>
   );
 }
