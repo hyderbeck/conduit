dcebcd2dc056532a699e4507c10a6a5772dbe83a
Hyder Beck <hyderbeck9@gmail.com>
2024-01-30 13:28:46 +0300

    feat: add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="flex-1 gap-x-4 flex items-center justify-center">
+      <h2 className="text-2xl font-bold pr-4 border-r border-emerald-300">
+        404
+      </h2>
+      <p className="text-sm">Page does not exist yet</p>
+    </main>
+  );
+}
